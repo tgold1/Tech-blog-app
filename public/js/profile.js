93ed1b2c8d49c44f3cfbeea3a166a4bdd8a96ad4
Tim Gold <timothygold@comcast.net>
2023-05-08 09:41:39 -0400

    added to dashboard.handlebars to get new posts when logged in

--- public/js/profile.js
+++ public/js/profile.js
@@ -27,7 +27,7 @@ const delButtonHandler = async (event) => {
   if (event.target.hasAttribute('data-id')) {
     const id = event.target.getAttribute('data-id');
 
-    const response = await fetch(`/api/Blogpost/${id}`, {
+    const response = await fetch(`/api/dashboard/${id}`, {
       method: 'DELETE',
     });
 
